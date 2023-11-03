7a7aaec2047f720bc0236d6a90145a6154722ec0
Kauã Villa Nova <kauavnds123@gmail.com>
2023-11-03 14:49:15 -0300

    Changing title and icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+  <>
+    <Head>
+      <title>Multi Step Form</title>
+      <link rel="icon" href="/assets/images/icon-checkmark.svg" sizes="any" />
+    </Head>
+    <Component {...pageProps} />
+  </>)
 }
